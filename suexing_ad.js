cde957b6735d678ffbb1ed154d95cc82625d75cd
42vio <viokeo@gmail.com>
2022-07-26 10:46:53 +0800

    Update suexing_ad.js

--- suexing_ad.js
+++ suexing_ad.js
@@ -9,19 +9,18 @@ const postMethod = "POST";
 if (!body.Data) {
     console.log("body:" + $response.body);
     $notification.post(noticeTitle, "苏e行", "Data为空");
-} else if (url.indexOf("/bas/ad/v1/advertisement/getAds") !== -1 && method === postMethod) ){
-        console.log('苏e行-开屏页');
-        body='';
-    } else if (url.indexOf("bff/app/index/recommend") !== -1 && method === postMethod) {
-        console.log('首页商品推荐');
+} else if (url.indexOf("/bas/ad/v1/advertisement/getAds") !== -1 && method === postMethod) {
+    console.log('苏e行-开屏页');
+    body='';
 
-        let newDataList = fruits.body.result.dataList(0,3);
-        body.result.dataList = newDataList;
-        
-    } else {
-        $notification.post(noticeTitle, "App路径/请求方法匹配错误:", method + "," + url);
-    }
+} else if (url.indexOf("bff/app/index/recommend") !== -1 && method === postMethod) {
+    console.log('首页商品推荐');
+    let newDataList = fruits.body.result.dataList(0,2);
+    body.result.dataList = newDataList;
+    }else {
+    $no tification.post(noticeTitle, "App路径/请求方法匹配错误:", method + "," + url);
 }
+
 body = JSON.stringify(body);
 
 $done({
